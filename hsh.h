7a0085fdf648715b501b137b075f3aee5070a15e
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2023-10-19 11:26:42 +0100

    Update hsh.h

--- hsh.h
+++ hsh.h
@@ -1,5 +1,11 @@
 #ifndef HSH_H
 #define HSH_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/wait.h>
 
 void terminal(char **argv, char **env);
 char **parse_paths();
