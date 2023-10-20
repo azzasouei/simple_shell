88faba63dd29c31bf48673dffb7918833703e0b7
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2023-10-20 11:04:38 +0100

    Update terminal.c

--- terminal.c
+++ terminal.c
@@ -1,30 +1,21 @@
 #include "hsh.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdbool.h>
-#include <sys/wait.h>
 
 /**
-*terminal-This function simulates a basic terminal that reads user input,
-*interprets and executes commands, and provides feedback.
-*@argv: A pointer to an array of strings representing command-line arguments.
-*@env: A pointer to an array of strings representing the environment variables.
-*Return: This function does not return a value (void).
-*/
+ *terminal-This function simulates a basic terminal that reads user input,
+ *interprets and executes commands, and provides feedback.
+ *@argv: A pointer to an array of strings representing command-line arguments.
+ *@env: A pointer to an array of strings
+ *representing the environment variables.
+ *Return: This function does not return a value (void).
+ */
 void terminal(char **argv, char **env)
 {
 	char *st = NULL;
 	int idx = 0;
-	int status = 0;
 	size_t n = 0;
 	ssize_t number_of_caracters;
 	char *arg_arr[] = {NULL, NULL};
-	char **path_list;
-	char *new_path;
 	char *delim = " ";
-	pid_t child;
 
 	while (1)
 	{
@@ -32,10 +23,7 @@ void terminal(char **argv, char **env)
 			printf(":) ");
 		number_of_caracters = getline(&st, &n, stdin);
 		if (number_of_caracters == -1)
-		{
-			free(st);
-			exit(EXIT_FAILURE);
-		}
+			custom_free(st, EXIT_FAILURE);
 		while (st[idx])
 		{
 			if (st[idx] == '\n')
@@ -58,55 +46,56 @@ void terminal(char **argv, char **env)
 			++idx;
 			arg_arr[idx] = strtok(NULL, delim);
 		}
-		child = fork();
-		if (child == -1)
-		{
-			free(st);
-			exit(EXIT_FAILURE);
-		}
-		else if (child == 0)
-		{
-			if (access(arg_arr[0], X_OK) == 0)
-			{
-				if (execve(arg_arr[0], arg_arr, env) == -1)
-					printf("%s :No such file or directory\n", argv[0]);
-			}
-			else
-			{
-				path_list = parse_paths();
-				if (path_list == NULL)
-					printf("%s :No such file or directory\n", argv[0]);
-				else
-				{
-					new_path = check_path(path_list, arg_arr[0]);
-					if (new_path == NULL)
-						printf("%s :No such file or directory\n", argv[0]);
-					else
-					{
-						if (execve(new_path, arg_arr, env) == -1)
-							printf("%s :No such file or directory\n", argv[0]);
-						free(new_path);
-					}
-					idx = 0;
-					while (path_list[idx] != NULL)
-					{
-						free(path_list[idx]);
-						++idx;
-					}
-					free(path_list);
-				}
-			}
-		}
-		else
-			wait(&status);
+		fork_function(st, argv, arg_arr, env, idx);
 	}
 }
+
+/**
+ * fork_function - This function simulates a basic terminal that
+ * interprets and executes commands, and provides feedback.
+ * @argv: A pointer to an array of strings representing arguments.
+ * @env: A pointer to an array of strings representing the environment.
+ * @st: A pointer to an array of strings representing arguments.
+ * @arg_arr: A pointer to an array of strings representing arguments.
+ * @id: A pointer to an array of strings representing arguments.
+ * Return: This function does not return a value (void).
+ */
+
+void fork_function(char *st, char **argv, char *arg_arr[], char **env, int id)
+{
+	char **path_list;
+	int status = 0;
+	pid_t child;
+
+	child = fork();
+	if (child == -1)
+	{
+		free(st);
+		exit(EXIT_FAILURE);
+	}
+	else if (child == 0)
+	{
+		if (access(arg_arr[0], X_OK) == 0)
+		{
+			if (execve(arg_arr[0], arg_arr, env) == -1)
+				printf("%s :No such file or directory\n", argv[0]);
+		}
+		else
+		{
+			path_list = parse_paths();
+			check_p_l(path_list, id, arg_arr, argv, env);
+		}
+	}
+	else
+		wait(&status);
+}
+
 /**
  * parse_paths-This function parses the PATH environment variable
  * and returns an array of directory paths.
  * Return: A pointer to an array of strings containing directory paths,
  * or NULL if there was an error in parsing or memory allocation.
-*/
+ */
 char **parse_paths()
 {
 	char **paths;
@@ -153,7 +142,7 @@ char **parse_paths()
  *@cmd: A string representing the command to search for in the directories.
  *Return: pointer to a string containing the full path to the command if found
  *or NULL if the command is not found in any of the directories.
-*/
+ */
 char *check_path(char **paths, char *cmd)
 {
 	int i = 0;
