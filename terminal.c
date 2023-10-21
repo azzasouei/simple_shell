c870d0c7b72462c2235a7b14d942b1c121b0da8e
Yosrahabouria <132350298+Yosrahabouria@users.noreply.github.com>
2023-10-21 21:34:48 +0100

    Update terminal.c

--- terminal.c
+++ terminal.c
@@ -98,6 +98,9 @@ void runcmd(char **rgv, char **arv, char **envp)
  */
 void exit_control(char *cmd)
 {
+	int i;
+	i = compare_string("exit", cmd);
+	printf("%i", i);
 	if (compare_string("exit", cmd) == 0)
 	{
 		free(cmd);
